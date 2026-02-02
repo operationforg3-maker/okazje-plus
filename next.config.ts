eb345b71973695d924d251e0c8e6b18dc38def5a
welldonetg974 <welldonetg974@gmail.com>
2026-02-02 18:30:04 +0100

    fix: allow convertiser image proxy host

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'imgproxy.convertiser.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
